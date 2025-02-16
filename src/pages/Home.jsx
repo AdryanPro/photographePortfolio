0bd882a8f33864b44a9446b19ddcf272cfb6a594
AdryanPro <adryan-pro@hotmail.com>
2025-02-16 16:29:15 +0100

    hEADER DONE

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,23 +4,29 @@ import zoomOnFace from '../assets/home/zoomOnFace.png'
 import scrollDown from '../assets/home/scrollDown.png'
 import "../css/home.css"
 export default function Home() {
+
   return (
     <div className='mainWrapperHome'>
         <header className='headerHome'>
-            <h1 className='h1TitleHome'>
-                Julien <br />
-                Pastouri
-            </h1>
-            <h2 className="underTitleHome">
-                PHOTOGRAPHER <br />
-                & VISUAL ARTIST
-            </h2>
-            <div className='fullbodyImgWrappe'>
-                <img src={photographeHeader} alt="" />
-                <img src={scrollDown} alt="" />
+            <div className="titleWrapper">
+                <h1 className='h1TitleHome'>
+                    Julien <br />
+                    <span className='moveTheWords'></span> Pastourri
+                </h1>
+                <h2 className="underTitleHome">
+                    PHOTOGRAPHER <br />
+                    <span className='moveTheWords'></span> & VISUAL ARTIST
+                </h2>
             </div>
-            <img src={zoomOnFace} alt="" />
+            <div className='fullbodyImgWrappe'>
+                <img className='photographeHeaderPic' src={photographeHeader} alt="" />
+                <img className='scrollDownPic' src={scrollDown} alt="" />
+            </div>
+            <div className="zoomfFaceWrapper">
+                <img src={zoomOnFace} alt="" />
+            </div>
         </header>
+
     </div>
   )
 }
